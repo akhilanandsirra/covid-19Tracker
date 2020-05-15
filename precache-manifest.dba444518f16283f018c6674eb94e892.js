669e06e87e9e5bb912dd59007dadad8e2fe2f163
akhilanandsirra <akhilanand9999@gmail.com>
2020-05-15 23:57:06 +0530

    Updates

---
+++ precache-manifest.dba444518f16283f018c6674eb94e892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bead47c470cc0fd76eaf288cdf295650",
+    "url": "/covid-19Tracker/index.html"
+  },
+  {
+    "revision": "b53e1c767b14dc45106c",
+    "url": "/covid-19Tracker/static/css/main.7768b127.chunk.css"
+  },
+  {
+    "revision": "858f8bcf0f9a92219766",
+    "url": "/covid-19Tracker/static/js/2.cc9ac2fe.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19Tracker/static/js/2.cc9ac2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53e1c767b14dc45106c",
+    "url": "/covid-19Tracker/static/js/main.6cb3e622.chunk.js"
+  },
+  {
+    "revision": "39399606a18719b96e6b",
+    "url": "/covid-19Tracker/static/js/runtime-main.f6c142fa.js"
+  }
+]);
